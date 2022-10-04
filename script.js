3f452595ee04beb214d6d697eeadef13da1888a2
Andres Laguilavo <lagui2003@gmail.com>
2022-10-04 12:18:51 -0500

    creacion de la logica en el script.js completamente funcional

---
+++ script.js
@@ -0,0 +1,51 @@
+let $ = (etiqueta) =>document.querySelector(etiqueta)
+
+let mes = $('.mes-numero')
+// let semana = $('.semana-numero')
+let dia = $('.dia-numero')
+let hora = $('.hora-numero')
+let minuto = $('.minuto-numero')
+let segundo = $('.segundo-numero')
+
+
+mesInicializador = 0
+diaInicializador = 0
+horaInicializador = 0
+minutoInicializador = 0
+segundoInicializador = 0
+
+
+setInterval(() => {
+
+    let hoy = new Date()
+    let navidad = new Date(2022, 11, 25)
+    mes.innerText = (navidad.getMonth() - hoy.getMonth())
+    dia.innerText = (navidad.getDate() - hoy.getDate())
+    hora.innerText = Math.abs((navidad.getHours() - hoy.getHours()))
+    minuto.innerText = (60 - hoy.getMinutes())
+    segundo.innerText = (60 - hoy.getSeconds())
+}, 1000);
+
+
+
+
+
+
+
+// setInterval(() => {
+//     mesInicializador += 1
+//     mes.innerText = mesInicializador
+//     console.log(mes);
+// }, 1);
+
+
+// setInterval(() => {
+//     let navidad = Date.parse("2022 12 24")
+//     let tiempoActual = Date.now()
+//     let tiempoParaNavidad = (navidad - tiempoActual)
+//     console.log({
+//         tiempoParaNavidad
+//         });
+    
+// }, 1000);
+
